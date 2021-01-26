ea78a30563ab5a345c4e017049100c63e8054d29
emanhamdy-a <0e0m0a0n0@gmail.com>
2021-01-26 13:41:05 +0200

    utf8

--- config/database.php
+++ config/database.php
@@ -15,7 +15,6 @@ return [
     |
     */
     'default' => env('DB_CONNECTION', 'pgsql'),
-    //'default' => env('DB_CONNECTION', 'mysql'),//'tenant',//
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +51,8 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
+            // 'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
